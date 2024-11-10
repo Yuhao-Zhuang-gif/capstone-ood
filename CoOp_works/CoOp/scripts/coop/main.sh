36122d2050ea734c023f381b6b30549d835471b7
Yuhao Zhuang <yz4619@columbia.edu>
2024-11-09 23:00:03 -0500

    Update main.sh comment

--- CoOp_works/CoOp/scripts/coop/main.sh
+++ CoOp_works/CoOp/scripts/coop/main.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # custom config
-DATA=../../DATA
+DATA=../../DATA 
 TRAINER=CoOp
 
+# bash command positions:
 DATASET=$1
-CFG=$2  # config file
+CFG=$2  # config file .yaml
 CTP=$3  # class token position (end or middle)
-NCTX=$4  # number of context tokens
-SHOTS=$5  # number of shots (1, 2, 4, 8, 16)
-CSC=$6  # class-specific context (False or True)
+NCTX=$4  # number of context tokens for each class
+SHOTS=$5  # number of shots (1, 2, 4, 8, 16), pictures for each class
+CSC=$6  # class-specific context (False or True), the other one is unified context
 
 for SEED in 1 2 3
 do
-    DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED}
+    DIR=output/${DATASET}/${TRAINER}/${CFG}_${SHOTS}shots/nctx${NCTX}_csc${CSC}_ctp${CTP}/seed${SEED} # ouput dir
     if [ -d "$DIR" ]; then
         echo "Oops! The results exist at ${DIR} (so skip this job)"
     else
