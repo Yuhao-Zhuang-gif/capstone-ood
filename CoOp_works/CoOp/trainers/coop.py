ad275081028a562aa84f10cd64a4028740051ece
Yuhao Zhuang <yz4619@columbia.edu>
2024-11-14 14:12:18 -0500

    Update coop.py comment

--- CoOp_works/CoOp/trainers/coop.py
+++ CoOp_works/CoOp/trainers/coop.py
@@ -19,7 +19,7 @@ _tokenizer = _Tokenizer()
 # load clip model
 def load_clip_to_cpu(cfg):
     backbone_name = cfg.MODEL.BACKBONE.NAME # CNN backbone, usually ResNet-50
-    url = clip._MODELS[backbone_name] # CoOp/clip/clip.py
+    url = clip._MODELS[backbone_name] # in CoOp/clip/clip.py, "https://openaipublic.azureedge.net/clip/models/afeb0e10f9e5a86da6080e35cf09123aca3b358a0c3e3b6c78a7b63bc04b6762/RN50.pt"
     model_path = clip._download(url) # CoOp/clip/clip.py
 
     try:
